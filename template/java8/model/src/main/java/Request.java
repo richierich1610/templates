88de897e6d46354f629f74da25d90791d4fa7e59
Alex Ellis <alexellis2@gmail.com>
2018-07-16 13:14:38 +0100

    Add Java8 template
    This adds the Java 8 template for OpenFaaS which uses gradle
    4.8.1 as a build system. This has been tested with community
    members and validated with Java developers. It mimicks the API
    of the other templates but is the first template to be included
    which leverages the of-watchdog and HTTP mode.
    
    A README.md is included for future maintenance.
    
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>
    
    request query params support added

---
+++ template/java8/model/src/main/java/Request.java
@@ -0,0 +1,52 @@
+// Copyright (c) OpenFaaS Author(s) 2018. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+package com.openfaas.model;
+
+import java.util.Map;
+
+public class Request implements IRequest {
+
+    private Map<String, String> headers;
+    private Map<String, String> queryParameters;
+    private String queryRaw;
+    private String body;
+
+    public Request(String body, Map<String, String> headers) {
+        this.body = body;
+        this.headers = headers;
+    }
+    
+    public Request(String body, Map<String, String> headers,Map<String, String> queryParameters,String queryRaw) {
+        this.body = body;
+        this.headers = headers;
+        this.queryParameters = queryParameters;
+        this.queryRaw = queryRaw;
+    }
+
+    public String getBody() {
+        return this.body;
+    }
+
+    public Map<String, String> getHeaders() {
+        return this.headers;
+    }
+
+    public String getHeader(String key) {
+        if(!this.headers.containsKey(key)) {
+            return null;
+        }
+
+        return this.headers.get(key);
+    }
+
+	@Override
+	public String getQueryRaw() {
+		return queryRaw;
+	}
+
+	@Override
+	public Map<String, String> getQueryParameters() {
+		return queryParameters;
+	}
+}
